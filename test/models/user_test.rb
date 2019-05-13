4a2a79b034c0c4f7e7bb32ed0c954eeb5311e1ad
YellowPipe <philippdmitrienko@gmail.com>
2019-05-13 14:53:41 +0300

    added user model tests and validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:one)
+  end	
+  
+  test 'Valid user' do
+	assert @user.valid?  
+  end
+
+  test 'Invalid without name' do
+  	@user.name = nil
+  	refute @user.valid?
+  	assert_not_nil @user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'Invalid user without email'	 do
+  	@user.email = nil
+  	refute @user.valid?
+  	assert_not_nil @user.errors[:email], 'no validation error for name present'
+  end
 end
