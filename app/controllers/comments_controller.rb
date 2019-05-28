51e665db194c07af2994a1d2124a00fef1236693
YellowPipe <philippdmitrienko@gmail.com>
2019-05-28 16:38:38 +0300

    comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if !@comment.save
+      flash[:error] = "Comment was not created"
+  	else
+  	  flash[:notice] = "Comment was successfully created."
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_back(fallback_location: root_path, notice: 'Comment was successfully deleted.')
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+  
+end
