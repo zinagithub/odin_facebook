710708a3d176f069680efb75d809042c24432171
YellowPipe <philippdmitrienko@gmail.com>
2019-05-29 14:07:53 +0300

    friends page and modified user views a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
-	before_action :set_user, only: [:show]
+	before_action :set_user, only: [:show, :friends]
 
 	def index
 		@users = User.all
 	end
 
 	def show
-		@posts = User.posts
+		@posts = @user.posts
+	end
+
+	def friends
+		@friends = @user.basic_friends + @user.inverse_friends
 	end
 
 	private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  get 'user/:id/friends', to: 'users#friends', as: 'user_friends'
   resources :users
   resources :posts
   root to: 'users#index'
