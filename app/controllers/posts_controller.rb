396ad3fbaebc053c1de4facbd43b2ddb357b75ef
Talaa Zina <talazina@gmail.com>
2019-05-29 11:32:15 +0100

    add methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,44 @@
 class PostsController < ApplicationController
+	
+	 before_action :authenticate_user!
+     before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def new
+  	@post = Post.all
+  end	
+  def show
+    @comments = @post.comments
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: @post.errors.full_messages.first
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+ 	
 end
+
