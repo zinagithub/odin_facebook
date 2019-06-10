f0dee278e2af7e353173f440b1200d6050c95adc
YellowPipe <philippdmitrienko@gmail.com>
2019-06-10 17:36:53 +0300

    added not_friends validation to model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,11 +2,16 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
   validate :not_self
+  validate :not_friends
 
   private
 
   def not_self
     errors.add(:friend, "can't be equal to user") if user == friend
   end
+
+  def not_friends
+  	errors.add(:friend, "already in friendlist") if user.friends.include?(friend)
+  end 
 
 end
