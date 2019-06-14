5aed5383e66affd9cce3ff68d5a0758de50c460a
YellowPipe <philippdmitrienko@gmail.com>
2019-06-14 13:37:16 +0300

    basic layout for devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
+Rails.application.config.assets.precompile += %w( devise.css )
+Rails.application.config.assets.precompile += %w( devise.js )
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
