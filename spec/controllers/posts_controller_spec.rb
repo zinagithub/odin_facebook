25d2d55cf4c219d9560ea7a64689af2db65e31e4
YellowPipe <philippdmitrienko@gmail.com>
2019-07-09 14:54:58 +0300

    removed tests for post show page

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,10 +17,6 @@ RSpec.describe PostsController, type: :controller do
 	      get :index
 	      expect(response).to render_template("index")
 	    end
-	    it "should renders the show template" do
-	      get :show, params: { id: post.id }
-	      expect(response).to render_template("show")
-	    end
 	    it "should renders the new template" do
 	      get :new
 	      expect(response).to render_template("new")
@@ -30,11 +26,6 @@ RSpec.describe PostsController, type: :controller do
 	context "Redirection" do
 	    it "redirects from index page to login_page" do
 	      get :index
-	      expect redirect_to new_user_session_path
-	    end
-
-	    it "redirects from show page to login_page" do
-	      get :show, params: { id: post.id }
 	      expect redirect_to new_user_session_path
 	    end
 	end
